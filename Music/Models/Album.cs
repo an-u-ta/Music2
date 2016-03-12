1055d8d34b246dbc8c31f633309bb96e151d9d28
annya_kiz <annya_kiz@hotmail.com>
2016-03-12 14:22:18 -0500

    ThirdChanges

--- Music/Models/Album.cs
+++ Music/Models/Album.cs
@@ -14,7 +14,10 @@ namespace Music.Models
         public string Title { get; set; }
         [Display(Name ="Genre")]
         public int GenreID { get; set; }
+        [Required, StringLength(20)]
         public Genre Genre { get; set; }
+        [Range(0.01, 100.0)]
+        [Required(ErrorMessage = "Price must be between 0.01 and 100")]
         public decimal Price { get; set; }
         [Display(Name = "Artist")]
         public int ArtistID { get; set; }
